8676d8f0f6311cdd3a1bf9a56965f4a8ef5b6157
vishamay123 <vishamay501@gmail.com>
2026-07-21 18:32:16 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/saloon-frontend/',
+  base: '/saloon-frontendk/', // અહીં છેલ્લે 'k' ઉમેર્યો છે
   server: {
     port: 3000
   }
